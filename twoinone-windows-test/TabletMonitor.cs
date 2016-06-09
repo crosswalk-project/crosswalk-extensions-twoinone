f346fe3c520a0f67e467c000925a5d64bbbad9da
Robert Staudinger <robert.staudinger@intel.com>
2016-06-09 21:27:26 +0200

    Add standalone test program

---
+++ twoinone-windows-test/TabletMonitor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xwalk
+{
+    class TabletMonitor : MonitorInterface
+    {
+        public ModeResponse MonitorTabletDelegate;
+        public delegate void ModeResponse(bool isTablet);
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int GetSystemMetrics(int nIndex);
+        private const int SM_CONVERTABLESLATEMODE = 0x2003;
+
+        private System.Threading.Timer _timer = null;
+        private bool _isTablet = false;
+
+        public TabletMonitor()
+        {
+        }
+
+        public bool IsTablet
+        {
+            get
+            {
+                bool tm = GetSystemMetrics(SM_CONVERTABLESLATEMODE) == 0;
+                if (_isTablet != tm)
+                {
+                    _isTablet = tm;
+                    MonitorTabletDelegate(_isTablet);
+                }
+                return _isTablet;
+            }
+        }
+
+        public void start()
+        {
+            _timer = new System.Threading.Timer(timerCb, null, 500, System.Threading.Timeout.Infinite);
+        }
+
+        public void stop()
+        {
+            _timer.Change(System.Threading.Timeout.Infinite, System.Threading.Timeout.Infinite);
+            _timer.Dispose();
+            _timer = null;
+        }
+
+        public bool isRunning()
+        {
+            return _timer == null;
+        }
+
+        private void timerCb(object state)
+        {
+            _isTablet = this.IsTablet;
+        }
+    }
+}
