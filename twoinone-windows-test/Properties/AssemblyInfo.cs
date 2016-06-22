52e196daeaf3f84817853457d8548de443a3f638
Robert Staudinger <robert.staudinger@intel.com>
2016-06-22 10:53:20 +0200

    Update license and copyright info

--- twoinone-windows-test/Properties/AssemblyInfo.cs
+++ twoinone-windows-test/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Reflection;
+﻿// Copyright (c) 2016 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
--- twoinone-windows/MonitorInterface.cs
+++ twoinone-windows/MonitorInterface.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright (c) 2016 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
